d4e080b21d9a7fb25db1999331eb391635726726
AndrewPari <77748463+AndrewPari@users.noreply.github.com>
2022-04-30 15:59:58 -0400

    added democracy score index & showed it with bar fill coloring

--- oil_bar_graph_race.js
+++ oil_bar_graph_race.js
@@ -1,65 +1,85 @@
 
-(function () {
-    //iife - this wraps the code in a function so it isn't accidentally exposed 
-    //to other javascript in other files. It is not required.
+var width = 2000, height = 800
 
-    var width = 2000, height = 800
+   
     
-    //read in our csv file 
-    d3.json("./data.json").then((data) => {
+//read in our json files
+d3.json("./data.json").then((data) => {
 
-        console.log(data);
+    /*
+    //filter out the nulls in price_usd
+    data = data.bitcoin.filter((d) => {
+        return d.price_usd;
+    })
+    */
 
-        const svg = d3.select("#canvas");
+    const oil = data;
 
-        //create an svg g element and add 50px of padding
-        const chart = svg.append("g")
-            .attr("transform", `translate(50, 50)`);
+    
+    const svg = d3.select("#canvas");
+    svg.style("background-color", "grey");
 
-
-        var x = d3.scaleBand()
-            .range([0, width])
-            .domain(data.map(function (d) { return d.Country; }))
-            .padding(0.1);
-
-
-        var y = d3.scaleLinear()
-            .domain([0, 2000000])
-            .range([height, 0]);
+    //create an svg g element and add 50px of padding
+    const chart = svg.append("g")
+        .attr("transform", `translate(50, 50)`);
 
 
-        bars = chart.append('g')
-            .selectAll(".rect")
-            .data(data)
-            .join("rect")
-            .attr("x", function (d) { return x(d.Country); })
-            .attr("y", function (d) { return y(d.zero); })
-            .attr("width", x.bandwidth())
-            .attr("height", function (d) { return height - y(d.zero); })
+    var x = d3.scaleBand()
+        .range([0, width])
+        .domain(oil.map(function (d) { return d.Country; }))
+        .padding(0.1);
 
 
-        //add axes
-        chart.append("g")
-            .attr("transform", "translate(0," + (height) + ")") //put our axis on the bottom
-            .call(
-                d3.axisBottom(x) //ticks + tickSize adds grids
-        ).call((g) => g.select(".domain").remove()) //Optional: remove the axis endpoints
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", function (d) {
-                return "rotate(-65)"
-            });
-
-        chart.append("g")
-            .call(
-                d3.axisLeft(y).ticks(10).tickSize(-width - 10)
-            ).call((g) => g.select(".domain").remove()); //Optional: remove the axis endpoints
+    var y = d3.scaleLinear()
+        .domain([0, 600000])
+        .range([height, 0]);
 
 
 
+    var blues = d3.scaleLinear()
+        .domain([0,5,10])
+        .range(["red", "white", "blue"]);
 
-    });
 
-})();
+
+    bars = chart.append('g')
+        .selectAll(".rect")
+        .data(oil)
+        .join("rect")
+        .attr("x", function (d) { return x(d.Country); })
+        .attr("y", function (d) { return y(d.ninetythree); })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) { return height - y(d.ninetythree); })
+        .attr("fill", function (d) {
+            if (d.Score) return blues(d.Score);
+            return 'black' })
+
+
+    //add axes
+    chart.append("g")
+        .attr("transform", "translate(0," + (height) + ")") //put our axis on the bottom
+        .call(
+            d3.axisBottom(x) //ticks + tickSize adds grids
+    ).call((g) => g.select(".domain").remove()) //Optional: remove the axis endpoints
+        .selectAll("text")
+        .style("text-anchor", "end")
+        .style("color", "white")
+        .attr("dx", "-.8em")
+        .attr("dy", ".15em")
+        .attr("font-size", "12px")
+        .attr("transform", function (d) {
+            return "rotate(-65)"
+        });
+
+    chart.append("g")
+        .style("color", "white")
+        .call(
+            d3.axisLeft(y).ticks(10).tickSize(-width - 10)
+        ).call((g) => g.select(".domain").remove()); //Optional: remove the axis endpoints
+
+
+
+
+});
+
+
